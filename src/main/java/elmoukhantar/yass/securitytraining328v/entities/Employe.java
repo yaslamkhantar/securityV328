db0931eeee437188d780b13e54bf4ac4746144f0
unknown <yassine.moukhantar002@gmail.com>
2024-08-18 17:39:54 +0200

    inmemory + authorisation -> done

--- src/main/java/elmoukhantar/yass/securitytraining328v/entities/Employe.java
+++ src/main/java/elmoukhantar/yass/securitytraining328v/entities/Employe.java
@@ -1,13 +1,25 @@
 package elmoukhantar.yass.securitytraining328v.entities;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Employe {
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/voiture")
     public String testWeb(){
         return "voiture";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/user")
+    public String test2(){
+        return "Megane";
+    }
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/admin")
+    public String test3(){
+        return "peugeot";
     }
 }
--- src/main/java/elmoukhantar/yass/securitytraining328v/filtrage/SecurityConfiguration.java
+++ src/main/java/elmoukhantar/yass/securitytraining328v/filtrage/SecurityConfiguration.java
@@ -1,16 +1,23 @@
 package elmoukhantar.yass.securitytraining328v.filtrage;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Bean
@@ -23,5 +30,12 @@ public class SecurityConfiguration {
        // http.formLogin(Customizer.withDefaults()); showing payload
        http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User.withUsername("hajar").password("{noop}lunix").roles("USER").build();
+        UserDetails admin1= User.withUsername("souad").password("{noop}solaris").roles("ADMIN").build();
+        UserDetails user3= User.withUsername("taha").password("xpppsjz").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user1, admin1, user3);
     }
 }
