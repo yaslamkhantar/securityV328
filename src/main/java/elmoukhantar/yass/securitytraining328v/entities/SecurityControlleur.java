e6369bb929b16b1c69974c906ff0e3415e8504a8
unknown <yassine.moukhantar002@gmail.com>
2024-08-18 17:46:16 +0200

    done2 -> succes

--- src/main/java/elmoukhantar/yass/securitytraining328v/entities/SecurityControlleur.java
+++ src/main/java/elmoukhantar/yass/securitytraining328v/entities/SecurityControlleur.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Employe {
+public class SecurityControlleur {
 
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/voiture")
-    public String testWeb(){
+    public String testglobal(){
         return "voiture";
     }
     @PreAuthorize("hasRole('ADMIN')")
